7c20e2a86ec8d7113a4e02cde04ac14d89fe8c11
BogdanK85 <kobakbogdan@gmail.com>
2023-09-23 12:20:40 +0300

    change + hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,61 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  onHandleFeedback = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  // };
+
+  //   countTotalFeedback = () => {
+  //     return Object.values(this.state).reduce((total, value) => total + value, 0);
+  // };
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((total, value) => total + value, 0);
+  const countPositiveFeedbackPercentage = () => {
+    const sum = countTotalFeedback();
+    return sum > 0 ? Math.round((good * 100) / sum) : 0;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const sum = this.countTotalFeedback();
-    return sum > 0 ? Math.round((this.state.good * 100) / sum) : 0;
+  const onHandleFeedback = option => {
+    if (option === 'good') setGood(prev => prev + 1);
+    if (option === 'neutral') setNeutral(prev => prev + 1);
+    if (option === 'bad') setBad(prev => prev + 1);
+    // this.setState(prevState => {
+    //   return {
+    //     [option]: prevState[option] + 1,
+    //   };
+    // });
   };
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <>
-        <Section title={'Please, leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onHandleFeedback}
+  // render() {
+  // const { good, neutral, bad } = state;
+  return (
+    <>
+      <Section title={'Please, leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onHandleFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        {countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title={'Statistics'}>
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        )}
+      </Section>
+    </>
+  );
+  // }
+};
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,14 +2,14 @@ import PropTypes from 'prop-types';
 import { SectionStyle, SectionTitle } from './Section.styled';
 
 export const Section = ({ title, children }) => {
-    return (
-        <SectionStyle>
-            <SectionTitle>{title}</SectionTitle>
-            {children}
-        </SectionStyle>
-    )
-}
+  return (
+    <SectionStyle>
+      <SectionTitle>{title}</SectionTitle>
+      {children}
+    </SectionStyle>
+  );
+};
 
 Section.propTipes = {
-    title: PropTypes.string
-}
+  title: PropTypes.string,
+};
